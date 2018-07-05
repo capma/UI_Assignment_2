d18196dee0b7581864237cbd17523638767cacb1
capma <boliquangtri@gmail.com>
2018-07-05 10:29:17 -0400

    Added new

---
+++ js/search.js
@@ -0,0 +1,27 @@
+
+  (function() {
+    var cx = '006155158425590177690:taz_rcx3dym';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+
+// jQuery( document ).ready(function() {
+// 	  var mark = function() {
+
+// 	    // Read the keyword
+// 	    var keyword = $("input[name='search']").val();
+
+// 	    // Remove previous marked elements and mark
+// 	    // the new keyword inside the context
+// 	    $(".panel-body").unmark({
+// 	      done: function() {
+// 	        $(".panel-body").mark(keyword, 'separateWordSearch');
+// 	      }
+// 	    });
+// 	  };
+// 	  $("input[name='search']").on("input", mark);
+// });
